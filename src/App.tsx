8e6676350873559c9c0d31b223ffbfcb0d908255
Praveen Kumar Jha <codewithkumar@gmail.com>
2026-02-12 07:58:35 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import Hero from './Common/Hero.tsx'
+
+function App() {
+   return(
+      <Hero/>
+   )
+}
+export default App
---
+++ src/Common/Hero.tsx
@@ -0,0 +1,15 @@
+import Header from "../Componenets/Header";
+function Hero() {
+  return (
+    <>
+    <div className="hero_area">
+      <div className="bg-box">
+        <img src="assets/images/hero-bg.jpg" alt=""></img>
+      </div>
+      <Header/>
+    </div> 
+    </>
+  );
+}
+
+export default Hero;
